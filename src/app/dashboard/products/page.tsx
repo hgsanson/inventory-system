9660917244ba55d02204c124a6524a6f2ab14d08
Holices <dev.holices@gmail.com>
2025-03-26 21:25:43 +0000

    feat: add check authentication

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,7 +1,57 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import DashboardLayout from "@/components/dashboard-layout";
+/* import ProductList from "@/components/product-list"; */
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default function Products() {
+export default function ProductsPage() {
 	const router = useRouter();
+	const searchParams = useSearchParams();
+	const companyId = searchParams.get("companyId");
+	const delegationId = searchParams.get("delegationId");
+	const branchId = searchParams.get("branchId");
+	const status = searchParams.get("status");
+	const month = searchParams.get("month");
+	const maintenanceMonth = searchParams.get("maintenanceMonth");
+	const category = searchParams.get("category");
+	const age = searchParams.get("age");
+	const region = searchParams.get("region");
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		// Otimizar a verificação de autenticação
+		const checkAuth = () => {
+			const isAuthenticated = localStorage.getItem("isAuthenticated");
+			if (!isAuthenticated) {
+				router.replace("/");
+			}
+		};
+
+		// Executar apenas no cliente
+		if (typeof window !== "undefined") {
+			checkAuth();
+		}
+	}, [router]);
+
+	return (
+		<DashboardLayout>
+			<div className="p-6">
+				<h1 className="text-2xl font-bold mb-6">
+					Gerenciamento de Equipamentos
+				</h1>
+				{/* 				<ProductList
+					initialCompanyFilter={companyId}
+					initialDelegationFilter={delegationId}
+					initialBranchFilter={branchId}
+					initialStatusFilter={status}
+					initialMonthFilter={month}
+					initialMaintenanceMonthFilter={maintenanceMonth}
+					initialCategoryFilter={category}
+					initialAgeFilter={age}
+					initialRegionFilter={region}
+				/> */}
+			</div>
+		</DashboardLayout>
+	);
 }
